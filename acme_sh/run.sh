d7521202ad817cbc7d558f7564de60ccaa13a74d
Eric Benzacar <benze@users.noreply.github.com>
2025-01-31 23:48:48 -0500

    Mount the /data folder so that the container has persistence between runs

--- acme_sh/run.sh
+++ acme_sh/run.sh
@@ -8,6 +8,9 @@ CERTFILE=$(bashio::config 'certfile')
 DNS_PROVIDER=$(bashio::config 'dns.provider')
 DNS_ENVS=$(bashio::config 'dns.env')
 DOMAIN_ALIAS=$(bashio::config 'domainalias')
+ACME_HOME=$(bashio::config 'data_folder' || echo "/data")
+
+export ACME_HOME
 
 for env in $DNS_ENVS; do
     export $env
@@ -38,5 +41,3 @@ $DOMAIN_ALIAS_ARG
 /root/.acme.sh/acme.sh --install-cert "${DOMAIN_ARR[@]}" \
 --fullchain-file "/ssl/${CERTFILE}" \
 --key-file "/ssl/${KEYFILE}" \
-
-tail -f /dev/null
